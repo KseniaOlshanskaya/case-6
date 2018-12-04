954ee2ff7bc4a25f7938c036d2731ddc2edc75c2
XeniaOlshanskaya <ks14012000@gmail.com>
2018-12-05 01:05:57 +0700

    Тесселяция + main2 (Арина сделала цвета)

--- case_6.py
+++ case_6.py
@@ -3,14 +3,14 @@ import turtle
 
 
 def tesselation(d, b, n, side_len):
-
+    _color_ch = ''
     color1 = get_color_choice(_color_ch)
     color2 = get_color_choice(_color_ch)
 
-    x = -250 + d
+    x = -250 + d + (d / 2)
     y = 250 + b
-    x1 = -250 - d
-    x2 = -250 + ((3 * d) / 2)
+    x1 = -250 + ((3 * d) / 2)
+    x2 = -250 + d
     counter = 0
     counter2 = 1
 
@@ -20,10 +20,18 @@ def tesselation(d, b, n, side_len):
     for s in range(n):
         counter += 1
         for i in range(n):
-            if counter2 % 2 == 0:
-                color = color1
-            else:
-                color = color2
+            if counter == 1 or counter == 2 or counter == 5 or counter == 6 or counter == 9 \
+                or counter == 10 or counter == 13 or counter == 14 or counter == 17 or counter == 18:
+                if counter2 % 2 != 0:
+                    color = color1
+                else:
+                    color = color2
+            elif counter == 3 or counter == 4 or counter == 7 or counter == 8 or counter == 11 \
+                or counter == 12 or counter == 15 or counter == 16 or counter == 19 or counter == 20:
+                if counter2 % 2 != 0:
+                    color = color2
+                else:
+                    color = color1
             turtle.up()
             x += d
             turtle.right(90)
