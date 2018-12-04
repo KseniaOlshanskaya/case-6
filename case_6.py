465307cbfd86083db9da990b78fa47a882287e4e
“ArinaZaytseva1904” <arina.zaytseva.2000@mail.ru>
2018-12-04 15:10:50 +0700

    proba

--- case_6.py
+++ case_6.py
@@ -1,1 +1,22 @@
 # Ветка Арины (фигура).
+from turtle import *
+n = int(input())
+d = 500//n
+a = (d // (3 ** 0.5))
+b = (d //(2 * (3 ** 0.5)))
+c = ((b * 2) + a)
+e = 500 // c
+arina_idealna = a
+right(90)
+forward(arina_idealna)
+right(60)
+forward(arina_idealna)
+right(60)
+forward(arina_idealna)
+right(60)
+forward(arina_idealna)
+right(60)
+forward(arina_idealna)
+right(60)
+forward(arina_idealna)
+mainloop()
